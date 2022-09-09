5315a9aa8da5fd90ad383f3ab370489ba620f06e
octokatt <katt@fatehold.com>
2022-09-09 16:18:36 -0700

    updated description gen

--- npc_description_gen.py
+++ npc_description_gen.py
@@ -4,30 +4,28 @@ import random
 with open("descriptors.json", "r") as read_file:
     descriptors = json.load(read_file)
 
-def appearance():
-    return "funny"
+def get_appearance():
+    rand = random.randint(0,len(descriptors["appearance"])-1)
+    return descriptors["appearance"][rand]
 
-def personality():
-    return "nervous"
-
-def interaction():
+def get_interaction():
     rand = random.randint(0,len(descriptors["interaction traits"])-1)
     return descriptors["interaction traits"][rand]
 
-def gender():
-    genders = ["male", "female", "non-binary"]
-    rand = random.randint(0,2)
-    if rand == 0:
-        return "man"
-    elif rand == 1:
-        return "woman"
-    else:
-        return "enby"
+def get_gender():
+    rand = random.randint(0,len(descriptors["genders"])-1)
+    return descriptors["genders"][rand]
+
+def get_quirk():
+    rand = random.randint(0,len(descriptors["talents"])-1)
+    return descriptors["talents"][rand]    
 
 def create_description():
  #   description = appearance() + personality() + interaction()
-    description = interaction() +" "+ gender()
+    description = "a "+ get_interaction() +" "+ get_gender()
+    description += " who looks "+ get_appearance()
+    description += " and is known for being " + get_quirk()
     return description
 
-current_description = create_description()
-print(current_description)
+# current_description = create_description()
+# print(current_description)
